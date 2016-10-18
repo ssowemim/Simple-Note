46f5caa074a97b9aa59bd72a0459133d730ee153
ssowemim <ssowemim@ualberta.ca>
2016-10-18 12:02:57 -0600

    adding initial files

---
+++ notes.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+
+var fetchNotes = () => {
+    try {
+      var notesString = fs.readFileSync('note-data.json');
+      return JSON.parse(notesString);
+    } catch (e) {
+      return [];
+    }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('note-data.json', JSON.stringify(notes));
+};
+
+var addNote = (title, body) => {
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+
+
+var duplicateNotes = notes.filter((note)  => note.title === title);
+  if (duplicateNotes.length === 0) {
+      notes.push(note);
+      saveNotes(notes);
+      return note;
+    }
+};
+
+var getAll = () => {
+  return fetchNotes();
+};
+
+var getNote = (title) => {
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => {
+    return note.title === title;
+  });
+  return filteredNotes[0];
+};
+
+var removeNote = (title) => {
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title !== title);
+  saveNotes(filteredNotes);
+
+  return notes.length !== filteredNotes.length
+};
+
+var logNote = (note) => {
+  console.log('--');
+  console.log(`Title: ${note.title}`);
+  console.log(`Body: ${note.body}`);
+};
+
+module.exports = {
+  addNote, //addNote: addNote
+  getAll,
+  getNote,
+  removeNote,
+  logNote
+};
